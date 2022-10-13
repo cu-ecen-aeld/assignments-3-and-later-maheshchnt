916ab56e3612d3aad07d285825bbd1b362f94267
maheshchnt <maheshchnt@gmail.com>
2022-10-12 18:04:56 -0600

    working changes of assignment 7 part one

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -14,7 +14,28 @@
 #include <string.h>
 #endif
 
+#include <stdio.h>
 #include "aesd-circular-buffer.h"
+
+
+/*
+ * @brief Private function used only by this .c file.
+ *
+ * Given the pointer of the queue, this function calculates next pointer value.
+ *
+ * @param ptr (in) - ptr of the messsage queue
+ *
+ * @return uint32_t (out) - next ptr value
+ */
+static uint32_t nextPtr(uint32_t ptr) {
+
+  if ((ptr + 1) >= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
+    return 0;
+  }
+
+  return (ptr+1);
+} // nextPtr()
+
 
 /**
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
@@ -29,6 +50,38 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
+    struct aesd_buffer_entry *entry = NULL;
+    int    char_pos = char_offset;
+    int    next_out_index = 0;
+
+    if (buffer != NULL) {
+	// read and write pointers are same but full flag is not set (empty condition)
+	if ((buffer->in_offs == buffer->out_offs) && (buffer->full == 0)) {
+	    printf("I shouldn't be coming here. IN:%d OUT:%d FULL:%d\n\r", buffer->in_offs, buffer->out_offs, buffer->full);
+            return NULL;
+	}
+
+	entry = &buffer->entry[buffer->out_offs];
+	next_out_index = nextPtr(buffer->out_offs);
+	
+	// iterate over entries until char pos is found
+	while (entry) {
+	    if (entry->size > char_pos) {
+		*entry_offset_byte_rtn = char_pos;
+		return entry;
+	    } else {
+		char_pos -= entry->size;
+		//read and write pointers are same-end of buffers
+                if (next_out_index == buffer->in_offs) {
+		    return NULL;
+		}
+		entry = &buffer->entry[next_out_index];
+                next_out_index = nextPtr(next_out_index);
+	    }
+
+	}
+
+    }
     /**
     * TODO: implement per description
     */
@@ -44,9 +97,28 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    int next_in_index;
+
+    if (buffer != NULL) {
+	//get the next write entry index
+	next_in_index = nextPtr(buffer->in_offs);
+
+	buffer->entry[buffer->in_offs] = *add_entry;
+
+	printf("written %s in %d offsetn\n\r", buffer->entry[buffer->in_offs].buffptr, buffer->in_offs);
+
+    	if (buffer->full) {//if full increment read pointer aswell
+	     buffer->out_offs = next_in_index;
+    	} else if (next_in_index == buffer->out_offs) {
+	     buffer->full = 1;
+    	}
+
+	//TODO: Debug print
+	printf("CUR_WR_PTR:%d NEXT_WR_PTR=%d CUR_RD_PTR=%d FULL=%d\n\r", buffer->in_offs, next_in_index, buffer->out_offs, buffer->full);
+
+
+	buffer->in_offs = next_in_index;
+    }
 }
 
 /**
